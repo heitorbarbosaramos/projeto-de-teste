a667b60ff9d9f184bcba84731df2a66293e7eb06
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-08-14 09:05:50 -0300

    Projeto criado

---
+++ src/main/java/com/heitor/projeto/ProjetoDeTestesApplication.java
@@ -0,0 +1,13 @@
+package com.heitor.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heitor/projeto/ProjetoDeTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitor.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
