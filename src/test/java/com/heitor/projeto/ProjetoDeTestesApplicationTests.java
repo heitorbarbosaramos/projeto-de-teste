02765f3eafb4de7bd358b519c7812212da1a6be8
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-08-15 17:34:35 -0300

    Aplicando teste para classe principal

--- src/test/java/com/heitor/projeto/ProjetoDeTestesApplicationTests.java
+++ src/test/java/com/heitor/projeto/ProjetoDeTestesApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ProjetoDeTestesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ProjetoDeTestesApplication.main(new String[] {});
 	}
 
 }
