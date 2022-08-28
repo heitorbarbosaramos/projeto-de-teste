18cb6a659df7186327d4aa47a1f2b180b5071669
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-08-28 19:33:47 -0300

    Implementando teste Buscar todos os usuarios

--- src/test/java/com/heitor/projeto/resources/UsuarioResourcesTest.java
+++ src/test/java/com/heitor/projeto/resources/UsuarioResourcesTest.java
@@ -1,5 +1,7 @@
 package com.heitor.projeto.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -32,69 +34,84 @@ public class UsuarioResourcesTest {
 	private UsuarioRepositoty repositoty;
 	@Mock
 	private UsuarioMapper usuMapper = UsuarioMapper.INSTANCE;
-	
+
 	private UsuarioDTO usuarioDto;
 	private Optional<Usuario> usuarioOptional;
-	
+	private List<UsuarioDTO> listUsuarioDto;
+
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
 		startEntidades();
 	}
-	
+
 	@Test
 	void quando_salvar_usuario() {
 		Mockito.when(usuarioService.newUser(Mockito.any())).thenReturn(usuarioDto);
 
 		ResponseEntity<UsuarioDTO> dto = resources.save(usuarioDto);
-		
+
 		Assertions.assertNotNull(dto);
 		Assertions.assertEquals(HttpStatus.CREATED, dto.getStatusCode());
-		
+
 	}
-	
+
 	@Test
 	void quando_chamar_find_by_id_com_retorno_sucesso() {
-		
+
 		Mockito.when(usuarioService.findById(Mockito.anyLong())).thenReturn(usuarioDto);
 		Mockito.when(repositoty.findById(Mockito.anyLong())).thenReturn(usuarioOptional);
 		Mockito.when(usuMapper.toDto(Mockito.any())).thenReturn(UsuarioBuilder.criarObjetDto());
-		
+
 		ResponseEntity<UsuarioDTO> response = resources.findById(UsuarioBuilder.ID);
-		
+
 		Assertions.assertNotNull(response);
 		Assertions.assertNotNull(response.getBody());
 		Assertions.assertEquals(ResponseEntity.class, response.getClass());
 	}
-	
+
 	@Test
 	void quando_atualizar() {
-		
+
 		Mockito.when(usuarioService.update(Mockito.anyLong(), Mockito.any())).thenReturn(usuarioDto);
-		
-		 ResponseEntity<UsuarioDTO> response = resources.update(1l, usuarioDto);
-		 
-		 Assertions.assertNotNull(response);
-		 Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-		 Assertions.assertEquals(UsuarioDTO.class, response.getBody().getClass());
-		
+
+		ResponseEntity<UsuarioDTO> response = resources.update(1l, usuarioDto);
+
+		Assertions.assertNotNull(response);
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Assertions.assertEquals(UsuarioDTO.class, response.getBody().getClass());
+
 	}
-	
+
 	@Test
 	void quando_deletar() {
-		
+
 		Mockito.doNothing().when(usuarioService).deleteUsuario(Mockito.anyLong());
-		
+
 		ResponseEntity<?> response = resources.deleteUsuario(1l);
-		
+
 		Assertions.assertNotNull(response);
 		Assertions.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
 
 	}
-	
+
+	@Test
+	void quando_buscar_todos() {
+
+		Mockito.when(usuarioService.findAll()).thenReturn(listUsuarioDto);
+
+		ResponseEntity<List<UsuarioDTO>> response = resources.findAll();
+
+		Assertions.assertNotNull(response);
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Assertions.assertEquals(ArrayList.class, response.getBody().getClass());
+	}
+
 	void startEntidades() {
-		usuarioDto 		= UsuarioBuilder.criarObjetDto();
+		usuarioDto = UsuarioBuilder.criarObjetDto();
 		usuarioOptional = UsuarioBuilder.criarOptional();
+
+		listUsuarioDto = new ArrayList<>();
+		listUsuarioDto.add(usuarioDto);
 	}
 }
-
