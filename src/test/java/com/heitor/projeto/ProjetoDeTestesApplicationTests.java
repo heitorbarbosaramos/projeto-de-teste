973e33efabc887339bf1688dfb5b6b1f9108b56b
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-08-28 13:03:36 -0300

    adcionando teste em profiles

--- src/test/java/com/heitor/projeto/ProjetoDeTestesApplicationTests.java
+++ src/test/java/com/heitor/projeto/ProjetoDeTestesApplicationTests.java
@@ -2,8 +2,10 @@ package com.heitor.projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ProjetoDeTestesApplicationTests {
 
 	@Test
